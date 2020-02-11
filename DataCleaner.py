cbb7aa8bd32ef428cdef755ed296d934735d5c58
rlcaust <Jayden@rlc.com.au>
2020-02-11 11:29:51 +1100

    Update DataCleaner.py

--- DataCleaner.py
+++ DataCleaner.py
@@ -13,5 +13,14 @@ from datetime import datetime as dt
 
 
 def DataClean(filename):
-	data = pd.read_csv(filename+ ".csv", sep=',', low_memory=False)
-	
+	data = pd.read_csv(filename + '.csv', sep=',', low_memory=False)
+	data = data.duplicates(subset=None, keep='first')
+
+	np.savetxt("Cleaned_Roles.csv", data, delimiter=",")
+
+	AssignmentTitle = data['Assignment Title']
+
+
+
+filename = "CSV_Roles"
+DataClean(filename)
