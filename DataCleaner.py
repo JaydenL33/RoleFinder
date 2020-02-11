ba69d8d6dbc2a889a3e905125011f002ebd50c92
rlcaust <Jayden@rlc.com.au>
2020-02-11 13:54:34 +1100

    Removed more duplicates
    More duplicates have been removed.

--- DataCleaner.py
+++ DataCleaner.py
@@ -1,5 +1,5 @@
 """
-Last Updated : 10/02/2020
+Last Updated : 11/02/2020
 Purpose: To modified CSV files and standardise CSVs.  
 
 Authors: Jayden Lee
@@ -14,20 +14,32 @@ from datetime import datetime as dt
 
 def DataClean(filename):
 	data = pd.read_csv(filename + '.csv', encoding = 'utf8')
-
+	# First Row Duplication Detection. 
 	data = data.drop_duplicates()
+	# Drops Un-nessecrary Columns
 	data = data.drop(["Skill & Proficiency", "Candidates"], axis=1)
-	pd.set_option('display.width', 10)
 
+	pd.set_option('display.width', 10) 
+	# ^ Print Display width for DF. 
+
+	# Duplication Detection.
 	indexAssignmentTitle = data[data['Assignment Title'].str.contains("Copy")].index
-
 	data.drop(indexAssignmentTitle, inplace=True)
+	
 
-	print(len(data))
+	indexDuplicated = data[data.duplicated()].index
+
+	# Duplication Deleted. 
+	
+	data.drop(indexDuplicated, inplace=True)
+	
+	# Debug Print
+	print("Length of Data is " + str(len(data)))
 	data.to_csv("Cleaned_Roles.csv", index=False)
+	print ("Success!")
 
 
 
 
-filename = "CSV_RolesFixed"
+filename = "CSV_Roles"
 DataClean(filename)
