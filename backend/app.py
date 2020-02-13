d7a965ff700caaa4ced8e70a5839bf3c0adeae07
rlcaust <Jayden@rlc.com.au>
2020-02-14 09:59:15 +1100

    Emptying again

--- backend/app.py
+++
@@ -1,9 +0,0 @@
-# Version 2.0
-# Created by Aaron Su 16 Dec 2019
-
-from src import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(debug=True)
--- backend/db_init.py
+++
@@ -1,5 +0,0 @@
-if __name__ == "__main__":
-    # Initialize database object
-    from src import db, create_app
-
-    db.create_all(app=create_app())
